0a1e7979ddd617f1aae5916b5b13deae32ee71c7
zhaige95 <zhaige95@163.com>
2019-05-04 02:33:48 +0800

    Start add battle kille msg list

---
+++ Assets/Resources/Script/UI/BattleStage/BattleKillListMgr.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleKillListMgr : MonoBehaviour {
+    public GameObject msgFriendly;
+    public GameObject msgEnemy;
+
+    public List<GameObject> msgList = new List<GameObject>();
+    public int maxMsgCount = 6;
+
+    public void AddKillMsg(string killer, string loser, bool isEnemy = false)
+    {
+        if (msgList.Count >= maxMsgCount)
+        {
+            Object.Destroy(msgList[0]);
+        }
+        var msgObj = GameObject.Instantiate(isEnemy ? msgEnemy : msgFriendly, this.transform);
+        msgObj.GetComponent<BattleKillMsgItem>().Init(killer, loser);
+        msgList.Add(msgObj);
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("j"))
+        {
+            this.AddKillMsg("如此良人何", "靶场机器人", false);
+        }
+    }
+}
---
+++ Assets/Resources/Script/UI/Parts/BattleKillMsgItem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleKillMsgItem : MonoBehaviour {
+
+    public Text killerText;
+    public Text LoserText;
+    public List<ContentSizeFitter> sizeFitters;
+    public float dieTime = 5f;
+
+    public void Init(string killer, string loser)
+    {
+        this.killerText.text = killer;
+        this.LoserText.text = loser;
+
+        foreach (var item in sizeFitters)
+        {
+            killerText.rectTransform.sizeDelta = new Vector2(20, killerText.rectTransform.sizeDelta.y);
+            LoserText.rectTransform.sizeDelta = new Vector2(20, LoserText.rectTransform.sizeDelta.y);
+            item.horizontalFit = ContentSizeFitter.FitMode.PreferredSize;
+        }
+
+        Object.Destroy(this.gameObject, dieTime);
+
+    }
+}
